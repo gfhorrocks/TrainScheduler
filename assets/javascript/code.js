eb14be0ca8aef7c110abfef99564787b86fad2b9
GregH <43968370+gfhorrocks@users.noreply.github.com>
2019-03-25 17:00:00 -0400

    required fields and color

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,20 +1,21 @@
 
-  var database = firebase.database();
-  
-  setInterval(update,1000);  //Sets interval to one second to update clock
+var database = firebase.database();
 
-  function update(){
-    $("#clock").html(moment().format("MMMM D YYYY hh:mm:ss A"));  // pulls time using moment and adds to clock div
-  }
-  
-  $("#addTrainButton").on("click", function(event) {
-    event.preventDefault();
-  
+setInterval(updateClock, 1000);  //Sets interval to one second to update clock
+
+function updateClock() {
+  $("#clock").html(moment().format("MMMM D YYYY hh:mm:ss A"));  // pulls time using moment and adds to clock div
+}
+
+$("#addTrainButton").on("click", function (event) {
+  event.preventDefault();
+  if ($("#trainNameInput").val() != '' && $("#destinationInput").val() != '' && $("#trainTimeInput").val() != '' && $("#frequencyInput").val() != '') {
+
     var trainName = $("#trainNameInput").val().trim();
     var destName = $("#destinationInput").val().trim();
     var firstTime = $("#trainTimeInput").val().trim();
     var frequencyTime = $("#frequencyInput").val().trim();
-  
+
     // Creates local "temporary" object for holding train data
     var newTrain = {
       name: trainName,
@@ -22,71 +23,84 @@
       time: firstTime,
       frequency: frequencyTime
     };
-  
+    console.log($("#trainNameInput").val());
     // Uploads train data to database (firebase)
-    database.ref().push(newTrain);    
+    database.ref().push(newTrain);
 
     // Clears all of the text-boxes
-    $("#trainNameInput").val("");
-    $("#destinationInput").val("");
-    $("#trainTimeInput").val("");
-    $("#frequencyInput").val("");
-  });
+    $("#trainNameInput").val("").attr("class","form-control bgwhite").attr("placeholder","");
+    $("#destinationInput").val("").attr("class","form-control bgwhite").attr("placeholder","");
+    $("#trainTimeInput").val("").attr("class","form-control bgwhite").attr("placeholder","");
+    $("#frequencyInput").val("").attr("class","form-control bgwhite").attr("placeholder","");
+  }
+  else{
+
+    $("#trainNameInput").val("").attr("class","form-control bgwhite");
+    $("#destinationInput").val("").attr("class","form-control bgwhite");
+    $("#trainTimeInput").val("").attr("class","form-control bgwhite");
+    $("#frequencyInput").val("").attr("class","form-control bgwhite");
+
+    if($("#trainNameInput").val()===""){$("#trainNameInput").attr("placeholder","*Required").attr("class","bgred form-control")};
+    if($("#destinationInput").val()===""){$("#destinationInput").attr("placeholder","*Required").attr("class","bgred form-control")};
+    if($("#trainTimeInput").val()===""){$("#trainTimeInput").attr("placeholder","*Required").attr("class","bgred form-control")};
+    if($("#frequencyInput").val()===""){$("#frequencyInput").attr("placeholder","*Required").attr("class","bgred form-control")};
   
-  database.ref().on("child_added", function(childSnapshot) {
+  }
+});
 
-    // Store everything into a variable.
-    var trainName = childSnapshot.val().name;
-    var destName = childSnapshot.val().destination;
-    var firstTime = childSnapshot.val().time;
-    var frequencyTime = childSnapshot.val().frequency;
-    var key = childSnapshot.key;
+database.ref().on("child_added", function (childSnapshot) {
 
-    // console.log(key);
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var destName = childSnapshot.val().destination;
+  var firstTime = childSnapshot.val().time;
+  var frequencyTime = childSnapshot.val().frequency;
+  var key = childSnapshot.key;
 
-    // Current Time
-    var currentTime = moment().format("HH:mm");
+  // console.log(key);
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
 
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  // Current Time
+  var currentTime = moment().format("HH:mm");
 
-    // Time apart (remainder)
-    var tRemainder = diffTime % frequencyTime;
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
-    // Minute Until Train
-    var tMinutesTillTrain = frequencyTime - tRemainder;
+  // Time apart (remainder)
+  var tRemainder = diffTime % frequencyTime;
 
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm A");
+  // Minute Until Train
+  var tMinutesTillTrain = frequencyTime - tRemainder;
 
-    // Create the new row
-    var newRow = $("<tr>").append(
-      $("<td>").text(trainName),
-      $("<td>").text(destName),
-      $("<td>").text(frequencyTime),
-      $("<td>").text(nextTrain),
-      $("<td>").text(tMinutesTillTrain),
-      $("<button class='removeButton' id="+key+">").text("X")
-    );
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm A");
 
-    newRow.attr("id",key);
+  // Create the new row
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(destName),
+    $("<td>").text(frequencyTime),
+    $("<td>").text(nextTrain),
+    $("<td>").text(tMinutesTillTrain),
+    $("<button class='removeButton' id=" + key + ">").text("X")
+  );
 
-    // Append the new row to the table
-    $("#trainData").append(newRow);
-  });
+  newRow.attr("id", key);
 
-$(document).on("click",".removeButton", function(){
-  
-  var key = $(this).attr("id");
-  console.log(key);
+  // Append the new row to the table
+  $("#trainData").append(newRow);
+});
 
-  dbRef = database.ref(key);
+$(document).on("click", ".removeButton", function () { //When a X button is pressed on a row
 
-  dbRef.remove();
+  var key = $(this).attr("id");   //Gets the "key" from the button ID
+  console.log(key);               //Logs the "key" value of Firebase object
 
-  $("#"+key).closest('tr').remove();
+  dbRef = database.ref(key);      //Creates a reference to the Firebase object
+
+  dbRef.remove();                 //Removes the object from Firebase
+
+  $("#" + key).closest('tr').remove();//Removes the table row from the screen
 
 });
-  
